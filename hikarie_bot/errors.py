37e57c64fc9619184711a112238603b66a68f3d3
Natsuki IWANO <n.iwano0630+github@gmail.com>
2024-09-20 01:28:18 +0000

    Refactor error message initialization in InvalidPointError class

--- hikarie_bot/errors.py
+++ hikarie_bot/errors.py
@@ -4,7 +4,7 @@ from typing import Self
 class InvalidPointError(Exception):
     """Raised when the point value is invalid."""
 
-    def __init__(self: Self, point: int, message: str = "Invalid point value") -> Self:
+    def __init__(self: Self, point: int, message: str = "Invalid point value") -> None:
         """Initialize the InvalidPointError class."""
         self.point = point
         self.message = message
