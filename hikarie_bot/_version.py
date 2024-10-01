8951e5764334414d915d48fcb21286daba97c184
Natsuki IWANO <n.iwano0630+github@gmail.com>
2024-10-01 09:01:19 +0000

    Refactor devcontainer.json and _version.py files

--- hikarie_bot/_version.py
+++ hikarie_bot/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev23+gb1e81d1.d20241001'
-__version_tuple__ = version_tuple = (0, 1, 'dev23', 'gb1e81d1.d20241001')
+__version__ = version = '0.1.dev24+g161dde7.d20241001'
+__version_tuple__ = version_tuple = (0, 1, 'dev24', 'g161dde7.d20241001')
