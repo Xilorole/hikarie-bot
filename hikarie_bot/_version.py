48e7f912d0e7620cb941c8b0cdbd8b01f3eb66a7
Natsuki IWANO <n.iwano0630+github@gmail.com>
2024-10-01 08:36:43 +0000

    Refactor .env.template for improved readability and maintainability

--- hikarie_bot/_version.py
+++ hikarie_bot/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev19+g29e9dbb.d20240930'
-__version_tuple__ = version_tuple = (0, 1, 'dev19', 'g29e9dbb.d20240930')
+__version__ = version = '0.1.dev23+gb1e81d1.d20241001'
+__version_tuple__ = version_tuple = (0, 1, 'dev23', 'gb1e81d1.d20241001')
