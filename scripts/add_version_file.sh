953f1a39a14f751e924bd8821e80b4f4c814f38b
Natsuki IWANO <n.iwano0630+github@gmail.com>
2024-10-15 02:43:14 +0000

    Refactor _version.py to update version number to '0.1.dev68+gd8bfa08.d20241015'

--- scripts/add_version_file.sh
+++ scripts/add_version_file.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Check if _version.py has been modified
-if git diff --name-only | grep -q "hikarie_bot/_version.py"; then
-    echo "_version.py has not been modified"
-else
-    echo "Staging _version.py"
+# Check if _version.py is not already staged
+if ! git diff --cached --name-only | grep -q "hikarie_bot/_version.py"; then
+    echo "_version.py is not staged. Running 'uv build'."
     uv build
-    git add hikarie_bot/_version.py
+
+    # Check if _version.py has been modified after running 'uv build'
+    if git diff --name-only | grep -q "hikarie_bot/_version.py"; then
+        echo "Staging _version.py"
+        git add hikarie_bot/_version.py
+    else
+        echo "_version.py has not been modified after 'uv build'"
+    fi
+else
+    echo "_version.py is already staged"
 fi
