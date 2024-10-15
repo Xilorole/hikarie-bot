86ec43b644fba6c07eee656df5c9798f3120a3ee
Natsuki IWANO <n.iwano0630+github@gmail.com>
2024-10-15 02:29:02 +0000

    Refactor check_version_update.sh script to always exit with error code 1

--- scripts/check_version_update.sh
+++ scripts/check_version_update.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-# Get the last commit hash
-last_commit=$(git rev-parse HEAD)
-
-# Check if hikarie_bot/_version.py was modified in the last commit
-if git diff-tree --no-commit-id --name-only -r $last_commit | grep -q "hikarie_bot/_version.py"; then
-    echo "Version file was updated in the last commit."
-    exit 0
-else
-    echo "Error: Version file was not updated in the last commit."
-    exit 1
-fi
+exit 1
