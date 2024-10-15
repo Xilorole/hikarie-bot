468f70b12dc29488ed59384c92b93d0dc50fd3c4
Natsuki IWANO <n.iwano0630+github@gmail.com>
2024-10-15 02:22:20 +0000

    Refactor pre-commit-config.yaml to add pre-push-uv-build hook

--- hikarie_bot/_version.py
+++ hikarie_bot/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev58+gb9d8ef5.d20241015'
-__version_tuple__ = version_tuple = (0, 1, 'dev58', 'gb9d8ef5.d20241015')
+__version__ = version = '0.1.dev61+gb17b348.d20241015'
+__version_tuple__ = version_tuple = (0, 1, 'dev61', 'gb17b348.d20241015')
