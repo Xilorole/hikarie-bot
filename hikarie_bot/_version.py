b17b348ffe9590ed78058a51224e856fc5753669
Natsuki IWANO <n.iwano0630+github@gmail.com>
2024-10-15 02:17:53 +0000

    updated version

--- hikarie_bot/_version.py
+++ hikarie_bot/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev53+g3cf688e.d20241009'
-__version_tuple__ = version_tuple = (0, 1, 'dev53', 'g3cf688e.d20241009')
+__version__ = version = '0.1.dev58+gb9d8ef5.d20241015'
+__version_tuple__ = version_tuple = (0, 1, 'dev58', 'gb9d8ef5.d20241015')
