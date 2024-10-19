b961723275d69bc826bedf61771f297473364775
Natsuki <n.iwano0630@gmail.com>
2024-10-19 12:57:37 +0000

    defined checker function

--- hikarie_bot/_version.py
+++ hikarie_bot/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev76+g9efd5cc.d20241018'
-__version_tuple__ = version_tuple = (0, 1, 'dev76', 'g9efd5cc.d20241018')
+__version__ = version = '0.1.dev77+gf06e012.d20241019'
+__version_tuple__ = version_tuple = (0, 1, 'dev77', 'gf06e012.d20241019')
