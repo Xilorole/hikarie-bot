0c1f77b49a0112825a4422e689c772f640d04895
Your Name <you@example.com>
2025-01-06 03:50:32 +0000

    added checkbox images

--- hikarie_bot/_version.py
+++ hikarie_bot/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev126+ge3b7ec2.d20241226'
-__version_tuple__ = version_tuple = (0, 1, 'dev126', 'ge3b7ec2.d20241226')
+__version__ = version = '0.1.dev130+g58bd39c.d20250106'
+__version_tuple__ = version_tuple = (0, 1, 'dev130', 'g58bd39c.d20250106')
