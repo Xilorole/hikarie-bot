372ab91490c9362285865ad03717f44ec08ca4f0
Your Name <you@example.com>
2025-01-07 01:50:01 +0000

    feat: update version and add badge image URLs with context item limit

--- hikarie_bot/_version.py
+++ hikarie_bot/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev130+g58bd39c.d20250106'
-__version_tuple__ = version_tuple = (0, 1, 'dev130', 'g58bd39c.d20250106')
+__version__ = version = '0.1.dev131+g0c1f77b.d20250107'
+__version_tuple__ = version_tuple = (0, 1, 'dev131', 'g0c1f77b.d20250107')
