8dac1f55de15a8eb7788db919a107e08ce631759
Xilorole <n.iwano0630+github@gmail.com>
2025-02-16 16:36:21 +0000

    chore: update Python version and dependencies to 3.12 and ruff to 0.9.6
    - Updated Python version requirement in pyproject.toml and workflows.
    - Bumped ruff dependency version in pyproject.toml and uv.lock.
    - Adjusted target version in ruff.toml to reflect Python 3.12.

--- hikarie_bot/_version.py
+++ hikarie_bot/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev146+g3b1cf2f.d20250216'
-__version_tuple__ = version_tuple = (0, 1, 'dev146', 'g3b1cf2f.d20250216')
+__version__ = version = '0.1.dev149+ge907f58.d20250216'
+__version_tuple__ = version_tuple = (0, 1, 'dev149', 'ge907f58.d20250216')
