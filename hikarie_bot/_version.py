af993f11c85200fc467b3881d81e877beefca9b1
Iwano-arise <natsuki.iwano@ariseanalytics.com>
2025-04-14 13:37:35 +0000

    Refactor code structure for improved readability and maintainability

--- hikarie_bot/_version.py
+++ hikarie_bot/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.dev163+g5d2655f.d20250414"
-__version_tuple__ = version_tuple = (0, 1, "dev163", "g5d2655f.d20250414")
+__version__ = version = '0.1.dev164+g84e639b.d20250414'
+__version_tuple__ = version_tuple = (0, 1, 'dev164', 'g84e639b.d20250414')
