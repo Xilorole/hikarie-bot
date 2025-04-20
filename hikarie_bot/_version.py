4a02f094839cd10e3e1abbc968d9a401ea32c024
Natsuki IWANO <n.iwano0630+git@gmail.com>
2025-04-20 12:19:52 +0000

    chore: replace custom Python setup action with astral-sh/setup-uv@v5

--- hikarie_bot/_version.py
+++ hikarie_bot/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev162+gebeb1c9.d20250418'
-__version_tuple__ = version_tuple = (0, 1, 'dev162', 'gebeb1c9.d20250418')
+__version__ = version = '0.1.dev163+ga1a8a7b.d20250420'
+__version_tuple__ = version_tuple = (0, 1, 'dev163', 'ga1a8a7b.d20250420')
