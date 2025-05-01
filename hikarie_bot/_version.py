08007f25ddf3bd7ecc71973ac25ec0d7182a1d6d
Iwano-arise <natsuki.iwano@ariseanalytics.com>
2025-05-01 07:30:46 +0000

    updated uv.lock

--- hikarie_bot/_version.py
+++ hikarie_bot/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.dev169+g79ff6e0.d20250501"
-__version_tuple__ = version_tuple = (0, 1, "dev169", "g79ff6e0.d20250501")
+__version__ = version = '0.1.dev170+g2658e5f.d20250501'
+__version_tuple__ = version_tuple = (0, 1, 'dev170', 'g2658e5f.d20250501')
