0e8c7fc5d861bd7795c1090c1145daa64e6c3fa0
Natsuki <n.iwano0630+github@gmail.com>
2025-07-28 15:45:43 +0900

    fix(Dockerfile): update CMD to include 'start' for jupyter hikarie_bot (#74)
    Co-authored-by: 岩野　夏樹 (ariseanalytics)_Ｄａｔａ＆ＡＩセンター <natsuki.iwano@ariseanalytics.com>

--- hikarie_bot/_version.py
+++ hikarie_bot/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev183+ga5aca11.d20250728'
-__version_tuple__ = version_tuple = (0, 1, 'dev183', 'ga5aca11.d20250728')
+__version__ = version = '0.1.dev181+g57a6be5.d20250728'
+__version_tuple__ = version_tuple = (0, 1, 'dev181', 'g57a6be5.d20250728')
