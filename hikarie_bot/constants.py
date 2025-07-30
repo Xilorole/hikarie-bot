3a801f6213649110a0c67fd53dfbc7373c9eaa46
Natsuki <n.iwano0630+github@gmail.com>
2025-07-30 20:52:46 +0900

    feat(kiriban): implement dynamic generation of kiriban badges and enhance AchievementView for kiriban badge handling (#81)
    * feat(kiriban): implement dynamic generation of kiriban badges and enhance AchievementView for kiriban badge handling
    
    - Introduced a KiribanGenerator class to dynamically generate kiriban badge data based on specific criteria.
    - Updated constants.py to replace static KIRIBAN_ID_COUNTS with a dynamically generated list.
    - Enhanced AchievementView to include logic for displaying kiriban badge status and next badge information.
    - Refactored badge handling methods to accommodate the new kiriban logic, improving performance and maintainability.
    
    * chore(version): update version to 0.1.dev186+g3242ae7.d20250730; refactor kiriban badge generation logic and improve test coverage
    
    ---------
    
    Co-authored-by: 岩野　夏樹 (ariseanalytics)_Ｄａｔａ＆ＡＩセンター <natsuki.iwano@ariseanalytics.com>

--- hikarie_bot/constants.py
+++ hikarie_bot/constants.py
@@ -1,50 +1,10 @@
 BADGE_TYPES_TO_CHECK: list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 15, 16, 17, 18]
-KIRIBAN_ID_COUNTS: list[tuple[int, int]] = [
-    (601, 100),
-    (602, 111),
-    (603, 200),
-    (604, 222),
-    (605, 300),
-    (606, 333),
-    (607, 400),
-    (608, 444),
-    (609, 500),
-    (610, 555),
-    (611, 600),
-    (612, 666),
-    (613, 700),
-    (614, 777),
-    (615, 800),
-    (616, 888),
-    (617, 900),
-    (618, 999),
-    (619, 1000),
-    (620, 1100),
-    (621, 1111),
-    (622, 1200),
-    (623, 1222),
-    (624, 1234),
-    (625, 1300),
-    (626, 1333),
-    (627, 1400),
-    (628, 1444),
-    (629, 1500),
-    (630, 1555),
-    (631, 1600),
-    (632, 1666),
-    (633, 1700),
-    (634, 1777),
-    (635, 1800),
-    (636, 1888),
-    (637, 1900),
-    (638, 1999),
-    (639, 2000),
-]
-
 
 ACHIEVED_BADGE_IMAGE_URL = "https://gist.github.com/user-attachments/assets/d9bddfb0-199c-4252-b821-52a62954811f"
 NOT_ACHIEVED_BADGE_IMAGE_URL = "https://gist.github.com/user-attachments/assets/86451acf-82ce-4ec2-b72e-7c6b9f728efb"
-TAKEN_6XX_BADGE_IMAGE_URL = "https://gist.github.com/user-attachments/assets/e71a6c8c-b28e-44dd-a4e5-b77395100998"
+TAKEN_6XXX_BADGE_IMAGE_URL = "https://gist.github.com/user-attachments/assets/e71a6c8c-b28e-44dd-a4e5-b77395100998"
 
 CONTEXT_ITEM_MAX = 10
 MAX_URL_DISPLAY_LENGTH = 100
+
+KIRIBAN_GENERATION_LIMIT = 10000
